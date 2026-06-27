0316d2f78e0de94abef04fa22be95cc5c8ed29b5
Yuniko <002notsans+1@gmail.com>
2026-06-27 02:11:05 +0200

    Delete assets/djs-selfbot-v13 directory

--- assets/djs-selfbot-v13/src/client/websocket/handlers/GUILD_JOIN_REQUEST_UPDATE.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = (client, packet) => {
-  // UPDATE uses the same action as CREATE (upsert into cache)
-  client.actions.GuildJoinRequestCreate.handle(packet.d);
-};
